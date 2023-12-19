7bc896ee3fd1d6ee45fbc9a89672a81449145ac4
Mihai Craiu <mihai.craiu@1and1.ro>
2023-12-19 21:32:08 +0200

    add APTX

--- build.sh
+++ build.sh
@@ -18,8 +18,15 @@ cd /tmp
 apt-get source bluez-alsa-utils
 
 cd bluez-alsa-*
+
+# AAC
 sed -ri 's/dh_auto_configure --/dh_auto_configure -- --enable-aac/' debian/rules
 sed -ri 's/^Build-Depends: /Build-Depends: libfdk-aac-dev, /g' debian/control
+
+# APTX
+sed -ri 's/dh_auto_configure --/dh_auto_configure -- --enable-aptx --enable-aptx-hd --with-libopenaptx/' debian/rules
+sed -ri 's/^Build-Depends: /Build-Depends: libopenaptx-dev, /g' debian/control
+
 dch -i "Enable extra codecs"
 dch -r ""
 echo | mk-build-deps -i
